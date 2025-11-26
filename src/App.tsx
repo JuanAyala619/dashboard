14e3aaeeafb80e1c98fd2f8ab9001a9514fae010
juaseaya <152338292+JuanAyala619@users.noreply.github.com>
2025-11-26 19:18:12 +0000

    guia13_completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 import './App.css'
 import { Grid } from '@mui/material';
+
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -12,19 +15,19 @@ function App() {
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
       {/* Encabezado */} {/* Json -> xs:12, md: 12*/}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
-
+      <Grid size={{ xs: 12, md: 12 }}><HeaderUI /> </Grid>
+      
       {/* Alertas */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+      <Grid size={{ xs: 12, md: 12 }} container justifyContent="right" alignItems="center"> <AlertUI description="No se preveen lluvias"/></Grid>
 
       {/* Selector */}
-      <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
 
       {/* Indicadores */}
       <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
       {/* Gráfico */}
-      <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
+      <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Gráfico</Grid>
 
       {/* Tabla */}
       <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
